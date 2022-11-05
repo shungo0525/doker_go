a889aebabe53451ade172293dbd9c7b58d2ba0ac
shungo0525 <shungo0525@gmail.com>
2022-11-05 10:47:47 +0900

    goでmysqlのデータを取得

--- app/main.go
+++ app/main.go
@@ -6,6 +6,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	Id     int
+	Name   string
+}
+
 func main() {
 	// [ユーザ名]:[パスワード]@tcp([dbのコンテナ名])/[データベース名]
 	dbconf := "root:password@tcp(mysql)/development"
@@ -26,4 +31,31 @@ func main() {
 	} else {
 			fmt.Println("データベース接続成功")
 	}
+
+	// ユーザーデータ取得 & データ出力
+	rows := getRows(db)
+	printUsers(rows)
+}
+
+func getRows(db *sql.DB) *sql.Rows {
+	rows, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		fmt.Println("Error")
+		panic(err.Error())
+	}
+	return rows
+}
+
+func printUsers(rows *sql.Rows) {
+	user := User{}
+	var result []User
+	for rows.Next() {
+		error := rows.Scan(&user.Id, &user.Name)
+		if error != nil {
+			fmt.Println("scan error")
+		} else {
+			result = append(result, user)
+		}
+	}
+	fmt.Println(result)
 }
