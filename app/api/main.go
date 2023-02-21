378d3971e4e6a7d408e2e1c907f75657537c4f99
shungo0525 <shungo0525@gmail.com>
2023-02-21 11:40:52 +0900

    net/httpでjsonを返す

--- app/api/main.go
+++ app/api/main.go
@@ -4,18 +4,41 @@ import (
   "fmt"
   "log"
   "net/http"
+  "encoding/json"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "Welcome to the HomePage!")
-  fmt.Println("Endpoint Hit: homePage")
+type User struct {
+	Id  int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func main() {
+  handleRequests()
 }
 
 func handleRequests() {
   http.HandleFunc("/", homePage)
-  log.Fatal(http.ListenAndServe(":8000", nil))
+  http.HandleFunc("/user", userPage)
+
+  err := http.ListenAndServe(":8000", nil)
+  if err != nil {
+    log.Fatal(err)
+  }
 }
 
-func main() {
-    handleRequests()
+func homePage(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "Home")
+  fmt.Println("Endpoint Hit: homePage")
+}
+
+func userPage(w http.ResponseWriter, r *http.Request){
+  user := User{1, "user-1"}
+  res, err := json.Marshal(user)
+  if err != nil {
+    panic(err)
+  }
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(res)
+
+  fmt.Println("Endpoint Hit: userPage")
 }
