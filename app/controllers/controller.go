ca79ac8b566814053199a5e69925cff56d39b9b8
shungo0525 <shungo0525@gmail.com>
2023-02-25 19:23:02 +0900

    不要なコード削除

--- app/controllers/controller.go
+++ app/controllers/controller.go
@@ -14,7 +14,6 @@ import (
 
 func HandleRequests() {
   http.HandleFunc("/", homePage)
-  http.HandleFunc("/user", userPage)
   http.HandleFunc("/send_email", sendEmail)
   http.HandleFunc("/get_parameters", getParameters)
   http.HandleFunc("/users/", handleUsersRequest)
@@ -90,18 +89,6 @@ func putUser(w http.ResponseWriter, r *http.Request) {
 func homePage(w http.ResponseWriter, r *http.Request){
   fmt.Fprintf(w, "Home")
   fmt.Println("Endpoint Hit: homePage")
-}
-
-func userPage(w http.ResponseWriter, r *http.Request){
-  user := model.User{1, "user-1"}
-  res, err := json.Marshal(user)
-  if err != nil {
-    panic(err)
-  }
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(res)
-
-  fmt.Println("Endpoint Hit: userPage")
 }
 
 func getParameters(w http.ResponseWriter, r *http.Request) {
